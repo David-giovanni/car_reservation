3f0664c4f618769ae48ebffa10504b8665be173e
David-giovanni <113347274+David-giovanni@users.noreply.github.com>
2024-08-30 16:37:35 +0200

    test vercel

--- back/api/home.js
+++ back/api/home.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  password: "gio",
-  host: "localhost",
-  port: 5432,
-  database: "test",
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT || 5432, // Assurez-vous que le port est défini, le port par défaut de PostgreSQL est 5432
+  database: process.env.PG_DATABASE,
 });
 
 module.exports = async (req, res) => {
@@ -13,11 +13,9 @@ module.exports = async (req, res) => {
     try {
       const query = `SELECT * FROM reservations;`;
       const result = await pool.query(query);
-      const reservations = result.rows;
-
-      res.status(200).json(reservations);
+      res.status(200).json(result.rows);
     } catch (error) {
-      console.error(error);
+      console.error("Error fetching reservations:", error);
       res.status(500).json({ message: "Error fetching reservations." });
     }
   } else {
--- back/api/homePost.js
+++ back/api/homePost.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  password: "gio",
-  host: "localhost",
-  port: 5432,
-  database: "test",
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT || 5432, // Assurez-vous que le port est défini, le port par défaut de PostgreSQL est 5432
+  database: process.env.PG_DATABASE,
 });
 
 module.exports = async (req, res) => {
